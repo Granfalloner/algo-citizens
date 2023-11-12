b6ffe5077f57d3609ac9255ae5eea23943d3af52
Vasyl Myronchuk <granfalloner@gmail.com>
2023-11-12 19:13:30 +0100

    Implement helper util function to 'hexlify' binary data in smart contract. Used to hexlify voting keys.

--- backend/smart_contracts/algo_citizens/contract.py
+++ backend/smart_contracts/algo_citizens/contract.py
@@ -103,3 +103,61 @@ def get_vote(address: pt.abi.Address, proposalId: pt.abi.Uint64, *, output: Prop
         app.state.votes[key].store_into(output)
     )
     
+@app.external
+def empty(*, output: pt.abi.Uint8) -> pt.Expr:
+    return output.set(pt.Int(1))
+    
+@app.external
+# @app.external(authorize=bk.Authorize.holds_token(asset_id=Asset_ID))
+def get_vote_test(address: pt.abi.Address, proposalId: pt.abi.Uint64, *, output: pt.abi.String) -> pt.Expr:
+    data = pt.abi.String()
+    return pt.Seq( 
+        data.set(pt.Sha256(pt.Concat(address.get(), pt.Itob(proposalId.get())))),
+        output.set(hexlify(data))
+        # output.set(data)
+    )
+
+@pt.Subroutine(pt.TealType.bytes)
+def hexlify(data: pt.abi.String) -> pt.Expr:
+    result = pt.abi.String()
+    i = pt.ScratchVar(pt.TealType.uint64)
+    byte = pt.abi.Byte()
+    byteAsString = pt.abi.String()
+    high = pt.abi.Uint8()
+    low = pt.abi.Uint8()
+
+    # pt.Len(data.get())
+    return pt.Seq(
+        result.set(''),
+        pt.For(i.store(pt.Int(0)), i.load() < pt.Len(data.get()), i.store(i.load() + pt.Int(1))).Do(
+            pt.Seq(
+                byte.set(pt.GetByte(data.get(), i.load())),
+                high.set(pt.Div(byte.get(), pt.Int(16))),
+                low.set(pt.Mod(byte.get(), pt.Int(16))),
+                byteAsString.set(pt.Concat(to_hex(high), to_hex(low))),
+                result.set(pt.Concat(result.get(), byteAsString.get()))
+            )
+        ),
+        result.get()
+    )
+
+@pt.Subroutine(pt.TealType.bytes)
+def to_hex(input: pt.abi.Uint8):
+    return pt.Cond(
+        [input.get() == pt.Int(15), pt.Bytes('F')],
+        [input.get() == pt.Int(14), pt.Bytes('E')],
+        [input.get() == pt.Int(13), pt.Bytes('D')],
+        [input.get() == pt.Int(12), pt.Bytes('C')],
+        [input.get() == pt.Int(11), pt.Bytes('B')],
+        [input.get() == pt.Int(10), pt.Bytes('A')],
+        [input.get() == pt.Int(9), pt.Bytes('9')],
+        [input.get() == pt.Int(8), pt.Bytes('8')],
+        [input.get() == pt.Int(7), pt.Bytes('7')],
+        [input.get() == pt.Int(6), pt.Bytes('6')],
+        [input.get() == pt.Int(5), pt.Bytes('5')],
+        [input.get() == pt.Int(4), pt.Bytes('4')],
+        [input.get() == pt.Int(3), pt.Bytes('3')],
+        [input.get() == pt.Int(2), pt.Bytes('2')],
+        [input.get() == pt.Int(1), pt.Bytes('1')],
+        [input.get() == pt.Int(0), pt.Bytes('0')],
+    )
