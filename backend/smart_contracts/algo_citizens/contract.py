7f2d9371a238cc2c448a44a29d949ad069e717ca
Vasyl Myronchuk <granfalloner@gmail.com>
2023-11-12 15:06:49 +0100

    Change proposalId to int type. Add User and Vote named tuples along with Box storage and corresponding methonds to vote.

--- backend/smart_contracts/algo_citizens/contract.py
+++ backend/smart_contracts/algo_citizens/contract.py
@@ -1,37 +1,58 @@
 import beaker as bk
 import pyteal as pt
 from beaker.lib import storage
+import typing
 
 Asset_ID=pt.Int(1027)
 
 class Proposal(pt.abi.NamedTuple):
-    id: pt.abi.Field[pt.abi.String]
+    id: pt.abi.Field[pt.abi.Uint64]
     name: pt.abi.Field[pt.abi.String]
     author: pt.abi.Field[pt.abi.String]
     authorAddress: pt.abi.Field[pt.abi.Address]
     description: pt.abi.Field[pt.abi.String]
     isVotingActive: pt.abi.Field[pt.abi.Bool]
     yesVotes: pt.abi.Field[pt.abi.Uint64]
-    noVotes: pt.abi.Field[pt.abi.Uint64]
 
 
+class User(pt.abi.NamedTuple):
+    vote_count: pt.abi.Field[pt.abi.Uint64]
+    vote_power: pt.abi.Field[pt.abi.Uint64]
+    delegate: pt.abi.Field[pt.abi.Address]
+    
+
+class Vote(pt.abi.NamedTuple):
+    proposalId: pt.abi.Field[pt.abi.Uint64]
+    voter: pt.abi.Field[pt.abi.Address]
+
+    
 class AlgoState:
     proposal_counter = bk.GlobalStateValue(stack_type=pt.TealType.uint64, default=pt.Int(0))
-    proposals = storage.BoxMapping(pt.abi.String, Proposal)
+    proposals = storage.BoxMapping(pt.abi.Uint64, Proposal)
 
-    user_vote_count = bk.LocalStateValue(stack_type=pt.TealType.uint64, default=pt.Int(5))
-    user_vote_power = bk.LocalStateValue(stack_type=pt.TealType.uint64, default=pt.Int(1))
-
+    user_counter = bk.GlobalStateValue(stack_type=pt.TealType.uint64, default=pt.Int(0))
+    users = storage.BoxMapping(pt.abi.Address, User)
+    
+    votes = storage.BoxMapping(pt.abi.String, Vote)
 
 app = bk.Application("AlgoCitizens", state=AlgoState()) \
-    .apply(bk.unconditional_create_approval, initialize_global_state=True) \
-    .apply(bk.unconditional_opt_in_approval, initialize_local_state=True)
+    .apply(bk.unconditional_create_approval, initialize_global_state=True)
 
 
-# @app.external(authorize=bk.Authorize.holds_token(asset_id=Asset_ID))
+# @app.external
+# def testBox(name: pt.abi.String, value: pt.abi.String) -> pt.Expr:
+#     return pt.App.box_put(name=name.get(), value=value.get())
+
+# @app.external(read_only=True)
+# def readTestBox(name: pt.abi.String, *, output: pt.abi.String) -> pt.Expr:
+#     return pt.Seq(contents := pt.App.box_get(name.get()),
+#                   pt.Assert(contents.hasValue()),
+#                   output.set(contents.value()))
+
+
 @app.external
 # @app.external(authorize=bk.Authorize.holds_token(asset_id=Asset_ID))
-def add_proposal(id: pt.abi.String, name: pt.abi.String, author: pt.abi.String, description: pt.abi.String, *, output: Proposal) -> pt.Expr:
+def add_proposal(id: pt.abi.Uint64, name: pt.abi.String, author: pt.abi.String, description: pt.abi.String, *, output: Proposal) -> pt.Expr:
     authorAddress = pt.abi.Address()
     isVotingActive = pt.abi.Bool()
     yesVotes = pt.abi.Uint64()
@@ -39,21 +60,46 @@ def add_proposal(id: pt.abi.String, name: pt.abi.String, author: pt.abi.String, 
     proposal = Proposal()
 
     return pt.Seq(
-        pt.Assert(pt.Not(app.state.proposals[id.get()].exists())),
+        pt.Assert(pt.Not(app.state.proposals[id].exists())),
         app.state.proposal_counter.increment(),
         authorAddress.set(pt.Txn.sender()),
         isVotingActive.set(False),
         yesVotes.set(0),
         noVotes.set(0),
-        proposal.set(id, name, author, authorAddress, description, isVotingActive, yesVotes, noVotes),
-        app.state.proposals[id.get()].set(proposal),
-        app.state.proposals[id.get()].store_into(output)
+        proposal.set(id, name, author, authorAddress, description, isVotingActive, yesVotes),
+        app.state.proposals[id].set(proposal),
+        app.state.proposals[id].store_into(output)
     )
 
 @app.external(read_only=True)
-def read_proposal(id: pt.abi.String, *, output: Proposal) -> pt.Expr:
-    return app.state.proposals[id.get()].store_into(output)
+def read_proposal(id: pt.abi.Uint64, *, output: Proposal) -> pt.Expr:
+    return app.state.proposals[id].store_into(output)
 
 @app.external
-def hello(name: pt.abi.String, *, output: pt.abi.String) -> pt.Expr:
-    return output.set(pt.Concat(pt.Bytes("Hello, "), name.get()))
+# @app.external(authorize=bk.Authorize.holds_token(asset_id=Asset_ID))
+def vote(proposalId: pt.abi.Uint64, *, output: Proposal) -> pt.Expr:
+    vote = Vote()
+    key = pt.abi.String()
+    address = pt.abi.Address()
+
+    return pt.Seq(
+        key.set(pt.Sha256(pt.Concat(pt.Txn.sender(), pt.Itob(proposalId.get())))),
+        pt.Assert(pt.Not(app.state.votes[key].exists())),
+        address.set(pt.Txn.sender()),
+        vote.set(proposalId, address),
+        app.state.votes[key].set(vote),
+        app.state.votes[key].store_into(output)
+    )
+
+
+@app.external(read_only=True)
+# @app.external(authorize=bk.Authorize.holds_token(asset_id=Asset_ID))
+def get_vote(address: pt.abi.Address, proposalId: pt.abi.Uint64, *, output: Proposal) -> pt.Expr:
+    key = pt.abi.String()
+
+    return pt.Seq(
+        key.set(pt.Sha256(pt.Concat(pt.Txn.sender(), pt.Itob(proposalId.get())))),
+        pt.Assert(app.state.votes[key].exists()),
+        app.state.votes[key].store_into(output)
+    )
+    
